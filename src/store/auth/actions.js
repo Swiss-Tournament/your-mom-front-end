0ad88e6bb1a12e28c023af79a513de9a28672cbc
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-02 12:14:30 -0700

    Added auth actions hook

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,1 +1,29 @@
-// add action creators here
+import {useCallback} from "react";
+import {useDispatch} from "react-redux";
+import { LOGIN_START, LOGIN_SUCCESS, AUTH_ERROR } from "./actionTypes";
+//import axios helper
+
+export const useAuthActions = () => {
+   const dispatch = useDispatch();
+   
+   // const autoLogin = useCallback(() => {}, [dispatch]);
+
+   const login = useCallback((credentials) => {
+      dispatch({type: LOGIN_START});
+
+      //axios call to backend
+      axios.post("some url", credentials)
+         .then(response => {
+            dispatch({type: LOGIN_SUCCESS, payload: response});
+         })
+         .catch(error => {
+            console.error(error.response);
+            dispatch({type: AUTH_ERROR, payload: error});
+         });
+   }, [dispatch]);
+   
+   return {
+      // autoLogin,
+      login
+   }
+};
