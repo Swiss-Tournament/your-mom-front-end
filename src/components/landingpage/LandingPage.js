f54c48f7a28bb0bf978ec49ab308c6a5dd626c39
AislynnEdmiston <davidandaislynn@gmail.com>
2020-01-02 16:04:39 -0500

    imported and added images to cards

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import './landingpage.css'
+import Forest from '../../assets/Forest-Art-Theros.jpg'
+import Island from '../../assets/Island-2-Amonkhet-MtG-Art.jpg'
+import Plains from '../../assets/Plains-Art-Theros.jpg'
+import Swamp from '../../assets/Swamp-Art-Theros.jpg'
+
 
 export default function LandingPage() {
     return (
@@ -9,18 +14,22 @@ export default function LandingPage() {
                 <div className='events'>
                     <h2>Events</h2>
                     <div className='card left'>
+                        <img src={Island} alt="forest mtg card" class="image"/>
                         <h3>Upcoming Events</h3>
                     </div>
                     <div className='card left'>
+                        <img src={Swamp} alt="forest mtg card" class="image"/>
                         <h3>Register An Event</h3>
                     </div>
                 </div>
                 <div className='magic-info'>
                     <h2>Magic Info</h2>
                     <div className='card right'>
+                        <img src={Plains} alt="forest mtg card" class="image"/>
                         <h3>Magic News</h3>
                     </div>
                     <div className='card right'>
+                        <img src={Forest} alt="forest mtg card" class="image"/>
                         <h3>Card DataBase</h3>
                     </div>
                 </div>
