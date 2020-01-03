4ec8d0baf527707d010418f05763fa16d7a9a8e6
nick.durbin <nick.durbin@gmail.com>
2020-01-03 09:16:35 -0500

    Created a layout for the events page.

--- src/components/events/EventPage/EventPage.js
+++ src/components/events/EventPage/EventPage.js
@@ -1,18 +1,75 @@
 import React from 'react';
+import styled from 'styled-components';
 import EventInfo from './event-page-components/EventInfo';
 import EventRules from './event-page-components/EventRules';
 
 const EventPage = () => {
 	return (
-		<div className="event-info-page">
+		<EventWrapper>
 			<EventInfo 
                 admin="admin" 
                 eventDate='eventDate'
                 startTime='startTime'
                 location='location'/>
 			<EventRules format="format" />
-		</div>
+
+			<PlayersAndScores>
+				<PlayerList>
+					<h1>List of Players</h1>
+					<HR />
+				</PlayerList>
+				<Scoreboard>
+					<h1>This will be a scoreboard.</h1>
+					<HR />
+				</Scoreboard>
+			</PlayersAndScores>
+		</EventWrapper>
 	);
 };
 
 export default EventPage;
+
+const EventWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	margin: 1% auto;
+	width: 100%;
+	min-height: 90vh;
+	height 100%;
+`
+
+const PlayersAndScores = styled.div`
+	display: flex;
+	width: 100%;
+	min-height: 20vh;
+	height: 100%;
+	justify-content: space-evenly;
+	align-items: center;
+`
+
+const PlayerList = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	min-height: 10vh;
+	height: 100%;
+	border-right: 2px solid black;
+`
+
+const Scoreboard = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+`
+
+const HR = styled.hr`
+    width: 12%;
+    border: .3rem solid black;
+    border-radius: 20px;
+`
