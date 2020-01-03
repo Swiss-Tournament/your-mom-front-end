52514a5e798adbc2c366c57d50b3670a8d9d33b0
nick.durbin <nick.durbin@gmail.com>
2020-01-03 07:55:57 -0500

    Added a max players input and basic styling.

--- src/components/UserAccess/CreateEvent.js
+++ src/components/UserAccess/CreateEvent.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 // import { useSelector } from "react-redux";
-
+import styled from 'styled-components';
 import axios from "axios";
 
 const CreateEvent = () => {
@@ -23,10 +23,7 @@ const CreateEvent = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    axios
-      .post(
-        `https://magic-the-gathering-tournament.herokuapp.com/api/event/10 , event `
-      )
+    axios.post(`https://magic-the-gathering-tournament.herokuapp.com/api/event/ , event`)
       .then(res => {
         console.log("res", res);
       })
@@ -38,48 +35,109 @@ const CreateEvent = () => {
       date: " ",
       location: " ",
       eventNotes: " ",
-      //   lat: 0,
-      //   lng: 0,
       public: false
     });
   };
 
-  console.log("Create Event", newEvent);
-
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
+      <h1>Create an Event</h1>
+      <HR />
       <label>Event Name</label>
-      <input
+      <Input
         type="text"
         name="name"
         value={newEvent.name}
         onChange={handleChange}
+        required
       />
       <label>Event Date and Time</label>
-      <input
+      <Input
         type="text"
         name="date"
         value={newEvent.date}
         onChange={handleChange}
+        required
       />
       <label>Event Location</label>
-      <input
+      <Input
         type="text"
         name="location"
         value={newEvent.location}
         onChange={handleChange}
+        required
       />
       <label>Event Notes</label>
-      <input
+      <Input
         type="textarea"
         name="eventNotes"
         value={newEvent.eventNotes}
         onChange={handleChange}
       />
 
-      <button type="submit">Submit</button>
-    </form>
+      <label>Max Players</label>
+      <Input
+        type="text"
+        name="eventPlayers"
+        value={newEvent.players}
+        onChange={handleChange}
+        required
+      />
+
+      <Button type="submit">Submit</Button>
+    </Form>
   );
 };
 
 export default CreateEvent;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 5% auto;
+    padding: 5% 1%;
+    width: 30%;
+    min-height: 500px;
+    height: 100%;
+    border: none;
+    border-radius: 20px;
+    box-shadow: 1px 3px 5px;
+`
+
+const Input = styled.input`
+    max-width: 40rem;
+    width: 100%;
+    min-height: 4rem;
+    height: 100%;
+    padding: 1%;
+    margin: 1.5% auto;
+    font-size: 1.4rem;
+`
+
+const Button = styled.button`
+    max-width: 35rem;
+    width: 100%;
+    min-height: 4rem;
+    height: 100%;
+    padding: 1%;
+    margin: 1% auto;
+    font-size: 1.3rem;
+    font-weight: 600;
+    border: none;
+    border-radius: 5px;
+    background: #D9B64E;
+    cursor: pointer;
+
+    &:hover {
+        background: #D9BE3B;
+        transform: scale(1.03)
+    }
+`
+
+const HR = styled.hr`
+    width: 8%;
+    border: .3rem solid black;
+    border-radius: 20px;
+`
