5f252822d9e03f922c98c744b04b52578e573c70
Carlos <miklo.concepcion@gmail.com>
2020-01-03 02:32:36 -0600

    routing completed. app is up and running. going back to styled-components to finish styling

--- src/components/navigation/Nav.js
+++ src/components/navigation/Nav.js
@@ -21,7 +21,7 @@ import EventPage from "../events/EventPage/EventPage";
 import EventListings from "../events/EventListings";
 import Login from "../UserAccess/Login";
 import Logout from "./routes/UserLogOut";
-import Register from "../UserAccess/Registration"
+import Register from "../UserAccess/Registration";
 import CreateEvent from "../UserAccess/CreateEvent";
 
 const NewNav = styled(Nav)`
@@ -54,51 +54,49 @@ const Navigation = () => {
     <div>
       <NewNav>
         <NavItem>
-          <NewLinks href="/">Home</NewLinks>
+          <NewLinks href='/'>Home</NewLinks>
         </NavItem>
         <NavItem>
-          <NewLinks href="/team">Team</NewLinks>
+          <NewLinks href='/team'>Team</NewLinks>
         </NavItem>
         <NavItem>
           {/*  Commented out until link set up */}
           {/* {UserSignedIn && <NavLinks href="/events">Events</NavLinks>} */}
-          <NewLinks href="/events">Events</NewLinks>
+          <NewLinks href='/events'>Events</NewLinks>
         </NavItem>
         <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
           <NewDropToggle nav caret>
             {/* <FontAwesomeIcon icon={faUser} size="1x" color="white" /> */}
-            <FontAwesomeIcon icon={faHatWizard} size="1x" color="white" />
+            <FontAwesomeIcon icon={faHatWizard} size='1x' color='white' />
           </NewDropToggle>
           <DropdownMenu>
             {!UserSignedIn && (
-              <DropdownItem tag={Link} to="/login">
+              <DropdownItem tag={Link} to='/login'>
                 Login
               </DropdownItem>
             )}
-              // {/* )} */}
-              // {/* {UserSignedIn && ( */}
-              <DropdownItem tag={Link} to='/user/:id/my-events'>
-                My Events
-              </DropdownItem>
-              // {/* )} */}
 
-              <DropdownItem divider />
-            </DropdownMenu>
-          </Dropdown>
+            {/* {UserSignedIn && ( */}
+            <DropdownItem tag={Link} to='/user/:id/my-events'>
+              My Events
+            </DropdownItem>
 
-          {/* <NavLink to="/login">Login</NavLink> */}
-        </NewNav>
+            <DropdownItem divider />
+          </DropdownMenu>
+        </Dropdown>
 
-        <Switch>
-          <Route exact path='/' component={LandingPage} />
-          <Route exact path='/events' component={EventListings} />
-          <Route path='/events/:id' component={EventPage} />
-          <Route path='/login' component={Login} />
-          <Route path='/logout' component={Logout} /> 
-          <Route path='/register' component={Register} />
-          <Route path='/create-event' component={CreateEvent} /> 
-        </Switch>
-      
+        {/* <NavLink to="/login">Login</NavLink> */}
+      </NewNav>
+
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/events' component={EventListings} />
+        <Route path='/events/:id' component={EventPage} />
+        <Route path='/login' component={Login} />
+        <Route path='/logout' component={Logout} />
+        <Route path='/register' component={Register} />
+        <Route path='/create-event' component={CreateEvent} />
+      </Switch>
     </div>
   );
 };
