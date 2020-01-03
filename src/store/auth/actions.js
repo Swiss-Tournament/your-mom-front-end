2ad9b3d184258ef83c29a1d503465158aff20aa1
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-03 03:43:57 -0700

    Register User works with location data

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,7 +1,8 @@
 import {useCallback} from "react";
 import {useDispatch} from "react-redux";
 import { LOGIN_START, LOGIN_SUCCESS, LOGIN_END, SIGNUP_START, SIGNUP_SUCCESS, AUTH_ERROR } from "./actionTypes";
-import {axiosWithAuth, axios, getToken, setToken} from "../../utils/axiosWithAuth";
+import {axiosWithAuth, getToken, setToken} from "../../utils/axiosWithAuth";
+import axios from "axios";
 
 const useAuthActions = () => {
    const dispatch = useDispatch();
@@ -44,27 +45,45 @@ const useAuthActions = () => {
          });
    }, [dispatch]);
 
-   const register = useCallback((newUser) => {
+   const register = useCallback((newUser, history) => {
       dispatch({type: SIGNUP_START});
       let newUserExtended = {...newUser};
 
       if (newUserExtended.location) {
-         //https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=REACT_APP_API_KEY
+         console.log("User has a location");
          const address = newUser.location.replace(/\s+/g, "+");
-         axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_API_KEY}`)
-            .then(([result]) => {
-               newUserExtended.lat = result.geometry.location.lat;
-               newUserExtended.lng = result.geometry.location.lng;
+         const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_API_KEY}`;
+         // const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyDtF2pl_Eiizfpicdir5t49-PuP06mdqXA`;
+         console.log("the url: ", url);
 
+         //https://maps.googleapis.com/maps/api/geocode/json?address=18825+N+3rd+Drive&key=AIzaSyDtF2pl_Eiizfpicdir5t49-PuP06mdqXA
+
+         axios.get(url)
+         // axios.get("https://magic-the-gathering-tournament.herokuapp.com/api/event/location")
+            // .then(response => {
+            //    console.log(response);
+            // })
+            .then(response => {
+               console.log("Google API success\n", response);
+
+               newUserExtended.lat = response.data.results[0].geometry.location.lat;
+               newUserExtended.lng = response.data.results[0].geometry.location.lng;
+
+               console.log("Gunna send this: ", newUserExtended);
                return axiosWithAuth().post("/auth/register", newUserExtended)
             })
-            .then(authSuccess)
-            .then(({data: {id, username}}) => {
-               dispatch({type: SIGNUP_SUCCESS, payload: {id, username}});
+            .then(({data: {message, token}}) => {
+               console.log(message);
+               if (window.alert('Thank you for registering. Please login to your new account.'));
+               if (history) history.push("/login");
             })
+            // .then(authSuccess)
+            // .then(({data: {id, username}}) => {
+            //    dispatch({type: SIGNUP_SUCCESS, payload: {id, username}});
+            // })
             .catch(error => {
-               console.error(error.response)
-               dispatch({type: AUTH_ERROR, payload: error.response});
+               console.error(error);
+               dispatch({type: AUTH_ERROR, payload: error});
             })
       } else {
          axiosWithAuth()
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -34,5 +34,3 @@ export const axiosWithAuth = (token) => {
       }
    })
 }
-
-export const axios;
