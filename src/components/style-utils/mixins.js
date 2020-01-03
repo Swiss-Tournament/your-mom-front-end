3b9ff19cc62cd5b3bda865c498641df96fdc1d92
Carlos <miklo.concepcion@gmail.com>
2020-01-03 00:27:18 -0600

    working on button variable to use in the application. question is what styling convention are we are using? styled components, bootstrap, or vanilla css.

--- src/components/style-utils/mixins.js
+++ src/components/style-utils/mixins.js
@@ -25,17 +25,6 @@ export const customLayout = (
    align-items: ${align};
 `;
 
-// buttons
-// const Button = styled.button`
-//   color: palevioletred;
-//   font-size: 1em;
-//   margin: 1em;
-//   padding: 0.25em 1em;
-//   border: 2px solid ${bgColor};
-//   border-radius: 3px;
-// `;
-// div containers
-
 // export const layout = css`
 //    display: flex;
 //    flex-direction: ${props => setCssFromProps(props, "direction", "row")};
--- src/components/style-utils/variables.js
+++ src/components/style-utils/variables.js
@@ -69,3 +69,14 @@ export const colors = {
   linkVisited,
   linkHover
 };
+
+// buttons
+// const Button = styled.button`
+//   color: palevioletred;
+//   font-size: 1em;
+//   margin: 1em;
+//   padding: 0.25em 1em;
+//   border: 2px solid ${bgColor};
+//   border-radius: 3px;
+// `;
+// div containers
