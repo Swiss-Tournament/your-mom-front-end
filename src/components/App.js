135482aa84be5b22b7904b87f7b585d8962c0c4b
Carlos <miklo.concepcion@gmail.com>
2020-01-02 21:15:02 -0600

    establishing styles to be connected to the app. routing small components and taking component by component. Looking into card rendering for the components. real-lifelike look

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import ActionsContext from "../context/Actions";
 import useActions from "../store/useActions";
 
-function App() {
-   const actions = useActions();
+import LandingPage from "../components/landingpage/LandingPage";
 
-   return (
-      <ActionsContext.Provider value={actions}>
-         <div className="App">
-            <h1>Hello You</h1>
-         </div>
-      </ActionsContext.Provider>
-      
-   );
+function App() {
+  const actions = useActions();
+
+  return (
+    <ActionsContext.Provider value={actions}>
+      <div className='App'>
+        <h1>Hello You</h1>
+        <LandingPage />
+      </div>
+    </ActionsContext.Provider>
+  );
 }
 
 export default App;
