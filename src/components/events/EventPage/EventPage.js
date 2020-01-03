6a16a3748580524b0318e5115355fb0665953bf1
Michelle Scott <scottmm374@gmail.com>
2020-01-03 13:20:00 -0500

    Players listed on event page

--- src/components/events/EventPage/EventPage.js
+++ src/components/events/EventPage/EventPage.js
@@ -1,43 +1,53 @@
-import React, {useContext, useEffect} from 'react';
-import styled from 'styled-components';
-import EventInfo from './event-page-components/EventInfo';
-import EventRules from './event-page-components/EventRules';
-import {useSelector} from "react-redux";
-import ActionsContext from '../../../context/Actions';
+import React, { useContext, useEffect } from "react";
+import styled from "styled-components";
+import EventInfo from "./event-page-components/EventInfo";
+import EventRules from "./event-page-components/EventRules";
+import { useSelector } from "react-redux";
+import ActionsContext from "../../../context/Actions";
+import EventPlayers from "./event-page-components/EventPlayers";
 
-const EventPage = ({match: {params: {id}}}) => {
-   const {event} = useContext(ActionsContext);
+const EventPage = ({
+  match: {
+    params: { id }
+  }
+}) => {
+  const { event } = useContext(ActionsContext);
 
-   useEffect(() => {
-      event.fetchEventById(id);
-   }, []);
+  useEffect(() => {
+    event.fetchEventById(id);
+  }, []);
 
-   const data = useSelector(state => state.event.event);
+  const data = useSelector(state => state.event.event);
 
-	return (
-		<>
-		<EventWrapper>
-			<EventInfo 
-                admin="admin" 
-                eventDate='eventDate'
-                startTime='startTime'
-                location='location'
-								data={data}/>
-			<EventRules format="format" />
+  console.log(data, "eventpage");
 
-			<PlayersAndScores>
-				<PlayerList>
-					<h1>List of Players</h1>
-					<HR />
-				</PlayerList>
-				<Scoreboard>
-					<h1>This will be a scoreboard.</h1>
-					<HR />
-				</Scoreboard>
-			</PlayersAndScores>
-		</EventWrapper>
-		</>
-	);
+  return (
+    <>
+      <EventWrapper>
+        <EventInfo
+          admin="admin"
+          eventDate="eventDate"
+          startTime="startTime"
+          location="location"
+          data={data}
+        />
+        <EventRules format="format" />
+
+        <PlayersAndScores>
+          <PlayerList>
+            <h1>List of Players</h1>
+            <EventPlayers players={data.players} />
+            <HR />
+          </PlayerList>
+
+          <Scoreboard>
+            <h1>This will be a scoreboard.</h1>
+            <HR />
+          </Scoreboard>
+        </PlayersAndScores>
+      </EventWrapper>
+    </>
+  );
 };
 
 export default EventPage;
@@ -51,38 +61,38 @@ const EventWrapper = styled.div`
 	width: 100%;
 	min-height: 90vh;
 	height 100%;
-`
+`;
 
 const PlayersAndScores = styled.div`
-	display: flex;
-	width: 100%;
-	min-height: 20vh;
-	height: 100%;
-	justify-content: space-evenly;
-	align-items: center;
-`
+  display: flex;
+  width: 100%;
+  min-height: 20vh;
+  height: 100%;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 
 const PlayerList = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	min-height: 10vh;
-	height: 100%;
-	border-right: 2px solid black;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 10vh;
+  height: 100%;
+  border-right: 2px solid black;
+`;
 
 const Scoreboard = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
 
 const HR = styled.hr`
-    width: 12%;
-    border: .3rem solid black;
-    border-radius: 20px;
-`
+  width: 12%;
+  border: 0.3rem solid black;
+  border-radius: 20px;
+`;
--- src/components/events/EventPage/event-page-components/EventPlayers.js
+++ src/components/events/EventPage/event-page-components/EventPlayers.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const EventPlayers = () => {
-//Need to find out how player list can be accessed.
-    return(
-        <>
-        </>
-    )
-}
+const EventPlayers = props => {
+  //Need to find out how player list can be accessed.
+  console.log(props, "data players");
+  return (
+    <>
+      {props.players.map(player => (
+        <ul key={player.id}>
+          <li>{player}</li>
+        </ul>
+      ))}
+    </>
+  );
+};
 
-export default EventPlayers
+export default EventPlayers;
