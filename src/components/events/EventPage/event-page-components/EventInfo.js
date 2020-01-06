97408521b2413471c8dc3dc6f090697053d15c0d
ndurbin <nick.durbin@gmail.com>
2020-01-06 00:40:31 -0500

    Adjusted the info to add the timer.

--- src/components/events/EventPage/event-page-components/EventInfo.js
+++ src/components/events/EventPage/event-page-components/EventInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Moment from "moment";
+import EventTimer from './EventTimer';
 
 const EventInfo = props => {
   const newDate = Moment(`${props.data.date}`).format("MM DD, YYYY ");
@@ -12,22 +13,12 @@ const EventInfo = props => {
         <Paragraph>Event 2nd: {props.data.admins[1]} </Paragraph>
         <Paragraph>Name: {props.data.name}</Paragraph>
       </OrganizerWrapper>
+      <EventTimer />
       <LocationWrapper>
         <Paragraph>Event Date: {newDate}</Paragraph>
         <Paragraph>Start Time: {newTime}</Paragraph>
         <Paragraph>Address: {props.data.location.address}</Paragraph>
       </LocationWrapper>
-
-      {/* <OrganizerWrapper>
-                <Paragraph>Event Organizer: Judge Awesome</Paragraph>
-                <Paragraph>Event 2nd: Commander 2nd</Paragraph>
-                <Paragraph>Name: Brenda's French Soul Food</Paragraph>
-            </OrganizerWrapper>
-            <LocationWrapper>
-                <Paragraph>Event Date: Jan. 17, 2020</Paragraph>
-                <Paragraph>Start Time: 1:00PM EST</Paragraph>
-                <Paragraph>Address: 652 Polk St, San Francisco, CA 94102</Paragraph>
-            </LocationWrapper> */}
     </InfoWrapper>
   );
 };
