93bbddde55b3da51252926e2a80aa981675b25cf
ndurbin <nick.durbin@gmail.com>
2020-01-07 09:56:38 -0500

    Added a split style with an image and form style

--- src/components/UserAccess/CreateEvent.js
+++ src/components/UserAccess/CreateEvent.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { useSelector } from "react-redux";
 import styled from 'styled-components';
 import ActionsContext from '../../context/Actions';
+import MTG from '../../assets/mtg.jpg'
 
 const CreateEvent = () => {
   // const error = useSelector(state => state.event.error)
@@ -40,125 +41,147 @@ const CreateEvent = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <h1>Create an Event</h1>
-      <HR />
-      <label>Event Name</label>
-      <Input
-        type="text"
-        name="name"
-        value={newEvent.name}
-        onChange={handleChange}
-        required
-      />
-      <label>Event Date and Time</label>
-      <Input
-        type="text"
-        name="date"
-        value={newEvent.date}
-        onChange={handleChange}
-        required
-      />
-      <label>Event Location</label>
-      <Input
-        type="text"
-        name="location"
-        value={newEvent.location}
-        onChange={handleChange}
-        required
-      />
-      <label>Event Notes</label>
-      <Input
-        type="textarea"
-        name="eventNotes"
-        value={newEvent.eventNotes}
-        onChange={handleChange}
-      />
+    <FormContainer>
+      <Form onSubmit={handleSubmit}>
+        <h1>Create an Event</h1>
+        <HR />
+        <label>Event Name</label>
+        <Input
+          type="text"
+          name="name"
+          placeholder="Event Name"
+          value={newEvent.name}
+          onChange={handleChange}
+          required
+        />
+        <label>Event Date and Time</label>
+        <Input
+          type="text"
+          name="date"
+          placeholder="Event Date and Time"
+          value={newEvent.date}
+          onChange={handleChange}
+          required
+        />
+        <label>Event Location</label>
+        <Input
+          type="text"
+          name="location"
+          placeholder="Event Location"
+          value={newEvent.location}
+          onChange={handleChange}
+          required
+        />
+        <label>Event Notes</label>
+        <Input
+          type="textarea"
+          name="eventNotes"
+          placeholder="Event Notes"
+          value={newEvent.eventNotes}
+          onChange={handleChange}
+        />
+        <label>Max Players</label>
+        <Input
+          type="text"
+          name="players"
+          value={newEvent.players}
+          onChange={handleChange}
+          required
+        />
 
-      <label>Max Players</label>
-      <Input
-        type="text"
-        name="players"
-        value={newEvent.players}
-        onChange={handleChange}
-        required
-      />
+        <Select name="gameFormat" onChange={handleChange} required>
+          <option value="" disabled>Choose Format...</option>
+          <option value="Standard">Standard</option>
+          <option value="Modern">Modern</option>
+          <option value="Legacy">Legacy</option>
+          <option value="Vintage">Vintage</option>
+          <option value="Block">Block</option>
+          <option value="Pauper">Pauper</option>
+          <option value="Pioneer">Pioneer</option>
+        </Select>
 
-      <Select name="gameFormat" onChange={handleChange} required>
-        <option value="" disabled>Choose Format...</option>
-        <option value="Standard">Standard</option>
-        <option value="Modern">Modern</option>
-        <option value="Legacy">Legacy</option>
-        <option value="Vintage">Vintage</option>
-        <option value="Block">Block</option>
-        <option value="Pauper">Pauper</option>
-        <option value="Pioneer">Pioneer</option>
-      </Select>
-
-      <Button type="submit">Submit</Button>
-    </Form>
+        <Button type="submit">SUBMIT</Button>
+      </Form>
+      <ImageContainer />
+    </FormContainer>
   );
 };
 
 export default CreateEvent;
 
 const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    margin: 5% auto;
-    padding: 5% 1%;
-    width: 30%;
-    min-height: 500px;
-    height: 100%;
-    border: none;
-    border-radius: 20px;
-    box-shadow: 1px 3px 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 5% 1%;
+  width: 40%;
+  min-height: 500px;
+  height: 100%;
+  background: white;
 `
 
 const Input = styled.input`
-    max-width: 40rem;
-    width: 100%;
-    min-height: 4rem;
-    height: 100%;
-    padding: 1%;
-    margin: 1.5% auto;
-    font-size: 1.4rem;
+  max-width: 50rem;
+  width: 100%;
+  min-height: 4rem;
+  height: 100%;
+  padding: 0 2%;
+  margin: 1.5% auto;
+  font-size: 1.4rem;
+
+  &:focus {
+    outline: none;
+  }
 `
 
 const Select = styled.select`
-    max-width: 40rem;
-    width: 100%;
-    min-height: 4rem;
-    height: 100%;
-    padding: 1%;
-    margin: 1.5% auto;
-    font-size: 1.4rem;
+  max-width: 50rem;
+  width: 100%;
+  min-height: 4rem;
+  height: 100%;
+  padding: 1%;
+  margin: 1.5% auto;
+  font-size: 1.4rem;
+
+  
+  &:focus {
+    outline: none;
+  }
 `
 
 const Button = styled.button`
-    max-width: 35rem;
-    width: 100%;
-    min-height: 4rem;
-    height: 100%;
-    padding: 1%;
-    margin: 1% auto;
-    font-size: 1.3rem;
-    font-weight: 600;
-    border: none;
-    border-radius: 5px;
-    background: #D9B64E;
-    cursor: pointer;
+  max-width: 35rem;
+  width: 100%;
+  min-height: 4rem;
+  height: 100%;
+  padding: 1%;
+  margin: 1% auto;
+  font-size: 1.6rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 5px;
+  background: #D9B64E;
+  cursor: pointer;
 
-    &:hover {
-        background: #D9BE3B;
-        transform: scale(1.03)
-    }
+  &:hover {
+    background: #D9BE3B;
+    transform: scale(1.03)
+  }
 `
 
 const HR = styled.hr`
-    width: 8%;
-    border: .3rem solid black;
-    border-radius: 20px;
+  width: 8%;
+  border: .3rem solid black;
+  border-radius: 20px;
+`
+
+const ImageContainer = styled.div`
+  width: 60%;
+  background-image: url(${MTG});
+`
+
+const FormContainer = styled.div`
+  display: flex;
+  height: 100vh;
 `
